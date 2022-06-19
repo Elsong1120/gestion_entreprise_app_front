4a1948d02e66dc6f23dfa8bb3899c2df58fa56cb
Elsong1120 <elsondushime@gmail.com>
2022-06-19 12:15:29 +0200

    Setup vuetify and vuejs

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-app-bar app color="indigo" dark>
+        <v-app-bar-nav-icon
+            @click.stop="$emit('toggle-drawer')"
+        ></v-app-bar-nav-icon>
+        <v-toolbar-title>Company</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon @click="toggle_dark_mode">
+            <v-icon>mdi-theme-light-dark</v-icon>
+        </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    methods: {
+        toggle_dark_mode: function () {
+            this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+            localStorage.setItem(
+                "dark_theme",
+                this.$vuetify.theme.dark.toString()
+            );
+        },
+    },
+    mounted() {
+        const theme = localStorage.getItem("dark_theme");
+        if (theme) {
+            if (theme === "true") {
+                this.$vuetify.theme.dark = true;
+            } else {
+                this.$vuetify.theme.dark = false;
+            }
+        } else if (
+            window.matchMedia &&
+            window.matchMedia("(prefers-color-scheme: dark)").matches
+        ) {
+            this.$vuetify.theme.dark = true;
+            localStorage.setItem(
+                "dark_theme",
+                this.$vuetify.theme.dark.toString()
+            );
+        }
+    },
+};
+</script>
---
+++ resources/js/routes/index.js
@@ -0,0 +1,20 @@
+export default {
+    mode: "history",
+    routes: [
+        // {
+        //     path: '/',
+        //     name: 'home',
+        //     component: Homepage,
+        // },
+        // {
+        //     path: '/about',
+        //     name: 'about',
+        //     component: About,
+        // },
+        // {
+        //     path: '/contact',
+        //     name: 'contact',
+        //     component: Contact,
+        // },
+    ],
+};
